199f8d19d5444433a9bf56b0b85977af2834f61e
Anthony Maitz <anthonymaitz@gmail.com>
2026-04-19 22:18:50 -0700

    feat(server): register ExploreRoom, TurnRoom, CombatRoom

--- server/src/game-server.ts
+++ server/src/game-server.ts
@@ -1,7 +1,16 @@
 import { Server } from '@colyseus/core'
 import { BunWebSockets } from '@colyseus/bun-websockets'
+import { ExploreRoom } from './rooms/ExploreRoom'
+import { TurnRoom } from './rooms/TurnRoom'
+import { CombatRoom } from './rooms/CombatRoom'
 
 export function createGameServer(): Server {
   const transport = new BunWebSockets()
-  return new Server({ transport })
+  const server = new Server({ transport })
+
+  server.define('explore', ExploreRoom)
+  server.define('turn', TurnRoom)
+  server.define('combat', CombatRoom)
+
+  return server
 }
