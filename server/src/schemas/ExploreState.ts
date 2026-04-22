643c1525eaa1faf47aa85c4499dbb5cd0738d1f7
Anthony Maitz <anthonymaitz@gmail.com>
2026-04-22 14:10:55 -0700

    feat(server): add NpcEntity and DoorEntity to ExploreState

--- server/src/schemas/ExploreState.ts
+++ server/src/schemas/ExploreState.ts
@@ -1,4 +1,4 @@
-import { Schema, MapSchema, type } from '@colyseus/schema'
+import { Schema, MapSchema, ArraySchema, type } from '@colyseus/schema'
 
 export class PlayerPosition extends Schema {
   @type('number') x: number = 0
@@ -6,6 +6,24 @@ export class PlayerPosition extends Schema {
   @type('string') characterId: string = ''
 }
 
+export class NpcEntity extends Schema {
+  @type('string') id: string = ''
+  @type('string') name: string = ''
+  @type('string') role: string = ''
+  @type('number') x: number = 0
+  @type('number') y: number = 0
+}
+
+export class DoorEntity extends Schema {
+  @type('string') id: string = ''
+  @type('string') biomeId: string = ''
+  @type('string') label: string = ''
+  @type('number') x: number = 0
+  @type('number') y: number = 0
+}
+
 export class ExploreState extends Schema {
   @type({ map: PlayerPosition }) players = new MapSchema<PlayerPosition>()
+  @type([NpcEntity]) npcs = new ArraySchema<NpcEntity>()
+  @type([DoorEntity]) doors = new ArraySchema<DoorEntity>()
 }
