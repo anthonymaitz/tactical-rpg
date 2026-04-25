f1b01469ea3a0172e457c74714228adbf94145c4
Anthony Maitz <anthonymaitz@gmail.com>
2026-04-24 19:47:02 -0700

    feat(server): add EnemyEntity schema and enemies MapSchema to ExploreState

--- server/src/schemas/ExploreState.ts
+++ server/src/schemas/ExploreState.ts
@@ -22,8 +22,20 @@ export class DoorEntity extends Schema {
   @type('number') y: number = 0
 }
 
+export class EnemyEntity extends Schema {
+  @type('string') id: string = ''
+  @type('string') name: string = ''
+  @type('number') x: number = 0
+  @type('number') y: number = 0
+  @type('number') hp: number = 10
+  @type('number') maxHp: number = 10
+  @type('number') level: number = 1
+  @type('boolean') fromSpawnPoint: boolean = false
+}
+
 export class ExploreState extends Schema {
   @type({ map: PlayerPosition }) players = new MapSchema<PlayerPosition>()
   @type([NpcEntity]) npcs = new ArraySchema<NpcEntity>()
   @type([DoorEntity]) doors = new ArraySchema<DoorEntity>()
+  @type({ map: EnemyEntity }) enemies = new MapSchema<EnemyEntity>()
 }
